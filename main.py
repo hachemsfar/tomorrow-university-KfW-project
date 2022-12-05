fb087c00cfc98eb3d3dcac6f4660e1c779b710d9
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2022-12-05 02:01:55 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,143 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from PIL import Image
+import io
+
+from collections import defaultdict
+#Creating Map
+import folium
+from folium.plugins import MarkerCluster
+from folium import IFrame
+from streamlit_folium import st_folium
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            .row_heading.level0 {display:none}
+            .blank {display:none}
+            .dataframe {text-align: left !important}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+def data_visualization():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("tomorrow_university.PNG")
+    with col2:
+        st.image("kfw_logo.PNG")
+
+    data = pd.read_csv('Ladesaeulenregister_CSV.csv', skiprows=10, sep=';', decimal=',',
+                     encoding="ISO-8859-1", engine='python')
+    data_shape=data.shape
+    data_columns=data.columns.tolist()
+    
+    st.header("Data Source:")
+    st.subheader("Public charging infrastructure")
+    col1, col2, col3 = st.columns(3)
+    with col2:
+        st.image("Bundesnetzagentur_logo.svg.PNG")
+
+    st.header("Data Analysis")
+    List_states=list(data['Bundesland'].unique()) #["All"]+
+    State=st.selectbox('Pick State', List_states)
+    if(State!="All"):
+        data=data[data['Bundesland']==State]
+        
+    List_cities=list(data['Ort'].unique())
+    selected_cities=st.multiselect('Choose cities', List_cities)
+    if(selected_cities):
+        data=data[data['Ort'].isin(selected_cities)]
+        
+    data.drop(['Public Key1','Public Key2','Public Key3'],axis=1,inplace=True)
+    data.drop_duplicates(subset = ["Breitengrad","Längengrad"],keep = 'last',inplace=True)
+    st.write(data)
+
+    st.success("(Before filtering) The dataset contains "+str(data_shape[0])+" rows")  
+    st.success("(After filtering) The dataset contains "+str(data.shape[0])+" rows (or chargers)")  
+    st.success("There are "+str(data_shape[1])+" columns")  
+
+    st.subheader("Summary of a DataFrame")
+    buffer = io.StringIO()
+    data.info(buf=buffer)
+    s = buffer.getvalue()
+
+    st.text(s)
+
+    st.subheader("Null values per column")
+    data_null_values=data.isnull().sum()
+    data_null_values=data_null_values.rename("# null values per column")
+    st.write(data_null_values.sort_values(ascending=False))
+
+    dataD = data.describe(include='all')
+    for i,j in zip(dataD.dtypes.tolist(),dataD.dtypes.index.tolist()):
+        if(i=="object"):
+            dataD[j] = dataD[j].astype(str)
+
+    st.subheader("descriptive statistics of the dataframe")
+    st.write(dataD)
+
+    if(selected_cities):
+        st.subheader("Top operators in "+str(selected_cities)+","+str(State))
+    else:
+        st.subheader("Top operators in "+str(State))
+        
+    s1=data['Betreiber'].value_counts()
+
+    s2=(data['Betreiber'].value_counts(normalize=True).rename("Betreiber %")).apply(lambda x:x*100)
+    df_concat=pd.concat([s1, s2], axis=1)
+    st.write(df_concat)
+
+    sum_betreiber=df_concat['Betreiber'].sum()-df_concat['Betreiber'].head(7).sum()
+    
+    fig,ax=plt.subplots(figsize=(11,7))
+    ax.pie(df_concat['Betreiber'].head(7).tolist()+[sum_betreiber], labels=df_concat.head(7).index.values.tolist()+["others"], autopct='%1.1f%%',shadow=True, startangle=90)
+    st.pyplot(fig)
+
+    sum_betreiber=df_concat['Betreiber'].sum()-df_concat['Betreiber'].head(5).sum()
+
+    fig1,ax1=plt.subplots(figsize=(11,7))
+    ax1.bar(df_concat.head(5).index.values.tolist()+["others"], df_concat['Betreiber'].head(5).tolist()+[sum_betreiber],color='g', label='cos')
+    st.pyplot(fig1)
+
+    if(selected_cities):
+        st.subheader("# charging points in each station "+str(selected_cities)+","+str(State))
+    else:
+        st.subheader("# charging points in each station "+str(State))
+        
+    fig,ax=plt.subplots(figsize=(11,7))
+    ax.hist(data['Anzahl Ladepunkte'], data['Anzahl Ladepunkte'].nunique()*2)
+    st.pyplot(fig)
+        
+    if(selected_cities):
+        m = folium.Map(location=[51.104138, 10.180465], zoom_start=5.3, tiles="CartoDB positron")
+
+        tooltip = "Click me!"
+
+        for index,row in enumerate(data.drop_duplicates(subset = ["Breitengrad","Längengrad"],keep = 'last').itertuples()):
+            html="<b>Operator:</b> {0} |<b>Commissioning date:</b> {6}| <b>Adress:</b> {1} {2}, {3} {4} {5}".format(row.Betreiber, row.Straße, row.Hausnummer, row.Postleitzahl,row.Ort,row.Bundesland,row.Inbetriebnahmedatum)
+            try:
+                folium.Marker([float(row.Breitengrad[:-1].replace(",",".")), float(row.Längengrad[:-1].replace(",","."))], popup=html, tooltip=tooltip).add_to(m)
+            except:
+                print("")
+
+        st_folium(m, width=700, height=500)
+
+def prediction():
+    st.header("Prediction")
+    st.subheader("Data Preprocessing")
+    st.subheader(".....Prediction")
+    
+page_names_to_funcs = {
+"Data Visualization": data_visualization,
+"Prediction": prediction
+}
+
+demo_name = st.sidebar.selectbox("Choose the App", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
