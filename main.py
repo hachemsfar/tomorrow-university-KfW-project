d685a3853175e5fa38354f359c89573d3766c043
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2022-12-06 00:10:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -137,13 +137,21 @@ def data_visualization():
 
     
     st.subheader("How many person per charger")
+    
     st.write(data2['Bundesland'].value_counts(ascending=True))
     st.write(data2['Ort'].value_counts(ascending=True))
 
     table_MN = pd.read_html('https://www.citypopulation.de/en/germany/cities/')
-    st.write(table_MN[0][['Name','Area A (km²)','Population Estimate (E) 2021-12-31']])
-    st.write(table_MN[2][['Name','Population Estimate (E) 2021-12-31','Area']])
+
+    df_state=table_MN[0][['Name','Area A (km²)','Population Estimate (E) 2021-12-31']]
+    df_city=table_MN[2][['Name','Population Estimate (E) 2021-12-31','Area']]
     
+    df_state['Name'] =  df_state['Name'].apply(lambda x: re.sub(r'\([^)]*\)', '', str(x)))
+    df_city['Name'] = df_city['Name'].apply(lambda x: re.sub(r'\([^)]*\)', '', str(x)))
+
+    st.write(df_state)
+    st.write(df_city)
+            
     if(selected_cities):
         m = folium.Map(location=[51.104138, 10.180465], zoom_start=5.3, tiles="CartoDB positron")
 
